232e894770a1da8eb6d6d18f9d7ac24c7e2ff151
Wylgner Emiliano <wylgneremiliano@gmail.com>
2023-09-11 22:35:15 -0300

    feat: click

--- src/script.js
+++ src/script.js
@@ -17,8 +17,47 @@ function jump() {
   if (!dino.classList.contains("jump")) {
     dino.classList.add("jump");
     alreadyJump = true;
-    let audio = new Audio("pulo.mp3");
-    audio.play();
+    // let audio = new Audio("pulo.mp3");
+    // audio.play();
+    setTimeout(() => {
+      dino.classList.remove("jump");
+      alreadyJump = false;
+    }, 1100);
+  }
+}
+
+setInterval(() => {
+  let dinoBottom = parseInt(
+    window.getComputedStyle(dino).getPropertyValue("bottom")
+  );
+  let cactoLeft = parseInt(
+    window.getComputedStyle(cacto).getPropertyValue("left")
+  );
+
+  if (cactoLeft > 40 && cactoLeft < 270 && dinoBottom <= 50 && !alreadyJump) {
+    alert(`Game Over! Seu score foi: ${count}`);
+
+    if (count > currentRecorde) {
+      currentRecorde = count;
+      recorde.innerHTML = `RECORDE: ${count}`;
+    }
+    count = 0;
+  }
+
+  count++;
+  score.innerHTML = `SCORE: ${count}`;
+}, 10);
+
+document.addEventListener("click", () => {
+  jump2();
+});
+
+function jump2() {
+  if (!dino.classList.contains("jump")) {
+    dino.classList.add("jump");
+    alreadyJump = true;
+    // let audio = new Audio("pulo.mp3");
+    // audio.play();
     setTimeout(() => {
       dino.classList.remove("jump");
       alreadyJump = false;
